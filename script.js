cd2ac88a10e8f0964413b5fdfb73fbf329bd8159
Kiran Shahi <kiran.shahi.np@gmail.com>
2019-06-24 09:10:30 +0545

    version on manifest updated.

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
             this.copyCode();
         },
         notifactionDom: function () {
-            var div = document.createElement('div');
+            let div = document.createElement('div');
             div.setAttribute("id", "cccTost");
             document.body.appendChild(div);
         },
@@ -18,21 +18,21 @@
                     range.selectNode(this);
                     window.getSelection().removeAllRanges();
                     window.getSelection().addRange(range);
-                    document.execCommand("Copy") ? cobj.showMsg("Code copied successfully !") : cobj.showMsg("Opps!! some error occured while copying code snippet.");
+                    document.execCommand("Copy") ? cobj.showMsg("Code snippet copied successfully !") : cobj.showMsg("Opps!! some error occured while copying code snippet.");
                     window.getSelection().empty();
                 });
             });
         },
         showMsg: function (message) {
-            var x = document.getElementById("cccTost");
+            let x = document.getElementById("cccTost");
             x.className = "show";
             x.textContent = message;
             setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
         }
     };
-    function ClicCopy() { }
-    ClicCopy.prototype.initialize = function () {
+    function ClickCopy() { }
+    ClickCopy.prototype.initialize = function () {
         ccc.init();
     };
-    new ClicCopy().initialize();
+    new ClickCopy().initialize();
 })();
