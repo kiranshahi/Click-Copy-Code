2b0866359ec3767028d10e2897a169cea4715f6d
Kiran Shahi <kiranshahi@users.noreply.github.com>
2025-08-06 13:23:17 +0100

    chore: add background service worker

--- background.js
+++ background.js
@@ -1,33 +1,13 @@
-(function () {
-    "use strict";
-    let bw = {
-        init: function () {
-            this.contextMenuInit();
-        },
-        contextMenuInit: function () {
-            
-            chrome.runtime.onInstalled.addListener(async (details) => {
-                await chrome.contextMenus.create({
-                    id: 'ccc-copyCode',
-                    title: 'Copy Code',
-                    type: 'normal',
-                    contexts: ['all'],
-                });
-            });
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: "ccc-copyCode",
+    title: "Copy Code",
+    contexts: ["all"],
+  });
+});
 
-            chrome.contextMenus.onClicked.addListener((info, tab) => {
-                console.log(info);
-                switch (info.menuItemId) {
-                    case 'ccc-copyCode':
-                        console.log('ccc-copyCode is clicked');
-                        break;
-                }
-            });
-        },
-    };
-    function BGWorker() { }
-    BGWorker.prototype.initialize = function () {
-        bw.init();
-    };
-    new BGWorker().initialize();
-})();
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === "ccc-copyCode" && tab?.id) {
+    chrome.tabs.sendMessage(tab.id, { action: "copy-code" });
+  }
+});
